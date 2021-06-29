60505d9b0423e6b36b829a1b35d0a4ffe781011f
aejin.lee <aejin.lee@snplab.io>
2021-06-29 12:04:38 +0900

    form 유효성 확인 구문 추가

--- src/main/resources/static/js/todo.js
+++ src/main/resources/static/js/todo.js
@@ -1,9 +1,33 @@
+function resetData() {
+    $("input[name='title']").val("");
+    $("textarea").val("");
+}
+
+//등록하기
+$(".btn-wrap button:first-child").on("click", function() {
+    let title = $("input[name='title']").val();
+    let content = $("textarea").val();
+
+    if(!title){
+        alert("제목을 입력해주세요!");
+        $("input[name='title']").focus();
+    }else if(!content){
+        alert("내용을 입력해주세요!");
+        $("textarea").focus();
+    }else {
+        console.log("ajax로 데이터 보내기");
+        resetData();
+    }
+});
+
+//메인 화면으로 돌아가기
 $(".btn-wrap button:nth-child(2)").on("click", function(){
     const listUrl = $("#listUrl").attr("data-src");
     window.location.href = listUrl;
 });
 
+
+//내용 초기화
 $(".btn-wrap button:last-child").on("click", function(){
-    $("input[name='title']").val("");
-    $("textarea").val("");
+    resetData();
 });
